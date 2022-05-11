ca0d6276a00a3aab7e40ddee8bb2c64f9f28f4b6
Iván <60348712+ivanmp-lm@users.noreply.github.com>
2022-05-11 12:53:03 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+#Actualizar repos
+apt update
+#Instalar MariaDB
+apt -y install mariadb-server mariadb-client
+#Instalar Make y GCC (necesarios para los módulos de Perl)
+apt -y install git curl wget make cmake gcc make
+#Instalar Apache y módulos
+apt -y install libapache2-mod-perl2 libapache-dbi-perl libapache-db-perl libapache2-mod-php libarchive-zip-perl
+#Instalar PHP
+apt-get -y install php php-pclzip php-mysql php-zip php-gd php-mbstring php-curl php-xml php-soap
+#Instalar Perl
+apt -y install perl libxml-simple-perl libcompress-zlib-perl libdbi-perl libdbd-mysql-perl libnet-ip-perl libsoap-lite-perl libio-compress-perl libapache-dbi-perl  libapache2-mod-perl2 libapache2-mod-perl2-dev
+#Instalar módulos Perl
+cpan install XML::Entities
+perl -MCPAN -e 'install Mojolicious'
+perl -MCPAN -e 'install Switch'
+perl -MCPAN -e 'install Plack::Handler'
+#Asegurar que el módulo perl funcione en Apache
+a2enmod perl
+systemctl restart apache2
