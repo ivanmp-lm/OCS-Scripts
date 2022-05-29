42e650f6f72bdd43d6b8ca5ad1cc65b94e6e26c1
Iván <60348712+ivanmp-lm@users.noreply.github.com>
2022-05-29 18:17:08 +0200

    Update cliente-unix.sh

--- cliente-unix.sh
+++ cliente-unix.sh
@@ -9,7 +9,7 @@ apt update
 #Aprovisionar con los componentes necesarios
 apt-get -y install libmodule-install-perl libdata-uuid-perl libcrypt-ssleay-perl libnet-snmp-perl libproc-pid-file-perl libproc-daemon-perl net-tools smartmontools read-edid nmap libnet-netmask-perl libxml-simple-perl libnet-ip-perl make
 #Descargar cliente de OCS Inventory
-wget https://github.com/OCSInventory-NG/UnixAgent/releases/tag/v2.9.1
+wget https://github.com/OCSInventory-NG/UnixAgent/releases/download/v2.9.1/Ocsinventory-Unix-Agent-2.9.1.tar.gz
 #Desempaquetar e instalar cliente
 https://github.com/OCSInventory-NG/UnixAgent/releases/download/v2.9.1/Ocsinventory-Unix-Agent-2.9.1.tar.gz
 tar -zxf Ocs*
